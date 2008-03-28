73b7e33339bdb909a2edd2449edf7d1c3a7d37f6
dmitryame <dmitryame@aadcfab2-1a40-0410-a277-c55e9a7a18d8>
2008-03-28 00:17:22 +0000

    adding hooks for authorization
    git-svn-id: http://cleantogether.googlecode.com/svn/trunk@64 aadcfab2-1a40-0410-a277-c55e9a7a18d8

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,16 +2,27 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base  
+  before_filter :check_authorization
+  
+  
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_cleantogether_session_id'
   
   def check_authentication
+    puts "!!!!!!!!!!!!!!!!!!!! authentication !!!!!!!!!!!!!!!!!!!!!!! " + request.request_uri
+    
     unless session[:user_id]
       session[:return_to] = request.request_uri
       flash[:notice] =    "Please log in"      
       redirect_to :controller => "user", :action => "signin"
     end
+  end    
+  
+  def check_authorization
+    # now, that the request is authenticated, let's see if it's authorized as well
+     puts "???????????????????? authorization ???????????????????????? " + request.request_uri
   end
+
 
 end
 
