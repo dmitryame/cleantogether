2fc898689bafa2d0923a724e4dedcf63aa928073
dmitryame <dmitryame@aadcfab2-1a40-0410-a277-c55e9a7a18d8>
2008-03-29 17:00:35 +0000

    successfully sending a rest request to preallwoed service
    git-svn-id: http://cleantogether.googlecode.com/svn/trunk@65 aadcfab2-1a40-0410-a277-c55e9a7a18d8

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,10 +1,18 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+#require 'rest-open-uri'
+#require 'rexml-document'
+#require 'net/http'
+require 'net/https'
+
+#require 'uri'
+
 class ApplicationController < ActionController::Base  
   before_filter :check_authorization
-  
-  
+
+  BASE_URI = "https://www.preallowed.com/clients/2"
+  SUBJECT  = "4"
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_cleantogether_session_id'
   
@@ -19,8 +27,28 @@ class ApplicationController < ActionController::Base
   end    
   
   def check_authorization
-    # now, that the request is authenticated, let's see if it's authorized as well
      puts "???????????????????? authorization ???????????????????????? " + request.request_uri
+     url = URI.parse(BASE_URI + "/subjects/" + SUBJECT + "/has_access/")
+      
+      
+     req = Net::HTTP::Post.new(url.path)
+     req.basic_auth 'preallowed_admin', 'preallowed_admin'
+     req.set_form_data({'reseource'=> request.request_uri}, ';')
+     # breakpoint
+     
+     http = Net::HTTP.new(url.host, url.port)
+     http.use_ssl = true
+     
+     res = http.start {|http| http.request(req) }
+     
+     case res
+     when Net::HTTPSuccess, Net::HTTPRedirection
+       puts "succsess!!!!!!!!!!!!!!!!!!!!!"
+       puts res.body
+     else
+       res.error!
+     end
+
   end
 
 
