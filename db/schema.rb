36a398866b85b72189489b9be7e814755cff1ae9
dmitryame <dmitryame@aadcfab2-1a40-0410-a277-c55e9a7a18d8>
2008-06-06 23:39:22 +0000

    facebooker plugin added
    git-svn-id: http://cleantogether.googlecode.com/svn/trunk@186 aadcfab2-1a40-0410-a277-c55e9a7a18d8

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(:version => 25) do
   end
 
   create_table "expeditions", :force => true do |t|
-    t.string   "name",            :default => "", :null => false
-    t.datetime "target_date",                     :null => false
-    t.integer  "captain_id",                      :null => false
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
-    t.integer  "geo_location_id",                 :null => false
+    t.string   "name",            :null => false
+    t.datetime "target_date",     :null => false
+    t.integer  "captain_id",      :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.integer  "geo_location_id", :null => false
     t.integer  "sponsor_id"
   end
 
@@ -41,12 +41,12 @@ ActiveRecord::Schema.define(:version => 25) do
   add_index "expeditions_teams", ["team_id"], :name => "index_expeditions_teams_on_team_id"
 
   create_table "geo_locations", :force => true do |t|
-    t.string   "name",                                        :default => "", :null => false
+    t.string   "name",                                        :null => false
     t.text     "description"
-    t.decimal  "lat",         :precision => 20, :scale => 15,                 :null => false
-    t.decimal  "lng",         :precision => 20, :scale => 15,                 :null => false
-    t.datetime "created_at",                                                  :null => false
-    t.datetime "updated_at",                                                  :null => false
+    t.decimal  "lat",         :precision => 20, :scale => 15, :null => false
+    t.decimal  "lng",         :precision => 20, :scale => 15, :null => false
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
   end
 
   add_index "geo_locations", ["lat"], :name => "index_geo_locations_on_lat"
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(:version => 25) do
   add_index "pictures", ["story_id"], :name => "index_pictures_on_cleaning_event_id"
 
   create_table "sponsors", :force => true do |t|
-    t.string   "name",       :default => "", :null => false
+    t.string   "name",       :null => false
     t.string   "url"
     t.string   "email"
     t.integer  "logo_id"
@@ -108,11 +108,11 @@ ActiveRecord::Schema.define(:version => 25) do
   add_index "stories", ["expedition_id"], :name => "index_cleaning_events_on_expedition_id"
 
   create_table "teams", :force => true do |t|
-    t.string   "name",       :default => "", :null => false
+    t.string   "name",       :null => false
     t.string   "motto"
-    t.integer  "captain_id",                 :null => false
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.integer  "captain_id", :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "teams", ["captain_id"], :name => "index_teams_on_captain_id"
