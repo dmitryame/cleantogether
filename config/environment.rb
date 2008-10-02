54fd17cb43669835af484250995f8efb4eb4af75
dmitryame <dmitryme@gmail.com>
2008-10-01 21:10:54 -0400

    trying to configure tls

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+# RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -84,6 +84,7 @@ HOST = 'http://localhost:3000'
 # config.active_record.observers = :user_observer
 # end
 
+require "smtp_tls"
 # Third, add your SMTP settings
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
